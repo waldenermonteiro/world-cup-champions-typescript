8ff5acd4349b408c70df9eafa501b619f5e6bc36
Waldener Monteiro <waldener.dev@gmail.com>
2021-10-03 00:34:55 -0300

    refactor: renaming utils

---
+++ src/utils/flags.js
@@ -0,0 +1,38 @@
+export const flags = [
+  {
+    name: 'Argentina',
+    source: require('../assets/flags/argentina.png'),
+  },
+  {
+    name: 'Brazil',
+    source: require('../assets/flags/brazil.png'),
+  },
+  {
+    name: 'England',
+    source: require('../assets/flags/england.png'),
+  },
+  {
+    name: 'France',
+    source: require('../assets/flags/france.png'),
+  },
+  {
+    name: 'Germany',
+    source: require('../assets/flags/germany.png'),
+  },
+  {
+    name: 'Italy',
+    source: require('../assets/flags/italy.png'),
+  },
+  {
+    name: 'Spain',
+    source: require('../assets/flags/spain.png'),
+  },
+  {
+    name: 'Uruguay',
+    source: require('../assets/flags/uruguay.png'),
+  },
+  {
+    name: 'West Germany',
+    source: require('../assets/flags/germany.png'),
+  },
+];
